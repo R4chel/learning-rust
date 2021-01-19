efb5ad2655360ed7730fc0e29adf74265059ca8a
R4chel <rachel@rachels-mbp.myfiosgateway.com>
2021-01-19 13:55:55 -0500

    start chapter 3 exercises

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// chapter 3 exercises
+
+fn fahrenheit_to_celcius(x:i32) -> i32 {
+    (x-32)*5/9 
+}
+
+fn print_fahrenheit_and_celcius(x: i32) {
+    println!("{} degrees fahrenheit is {} degrees celcius", x, fahrenheit_to_celcius(x));
+}
 fn main() {
-    println!("Hello, world!");
+    print_fahrenheit_and_celcius(0);
+    print_fahrenheit_and_celcius(32);
+    print_fahrenheit_and_celcius(-40);
 }
