e3ddb09730c0fded2a7ad694822505d2eb0cf471
R4chel <rachel@rachels-mbp.myfiosgateway.com>
2021-01-22 13:51:13 -0500

    implement fibonacci

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,31 @@ fn fahrenheit_to_celcius(x:i32) -> i32 {
 fn print_fahrenheit_and_celcius(x: i32) {
     println!("{} degrees fahrenheit is {} degrees celcius", x, fahrenheit_to_celcius(x));
 }
+
+fn fibonacci(n: i32) -> i32 {
+    if n <= 0 {
+        0
+    }
+    else if n == 1 {
+        1
+    }
+    else {
+        fibonacci(n-1) + fibonacci(n-2)
+    }
+} 
+ 
+fn print_fibonacci(n: i32) {
+    println!("The {}th fibonacci number is {}",n, fibonacci(n));
+}
 fn main() {
     print_fahrenheit_and_celcius(0);
     print_fahrenheit_and_celcius(32);
     print_fahrenheit_and_celcius(-40);
+    print_fibonacci(0);
+    print_fibonacci(1);
+    print_fibonacci(2);
+    print_fibonacci(3);
+    print_fibonacci(4);
+    print_fibonacci(10);
+    print_fibonacci(-10);
 }
